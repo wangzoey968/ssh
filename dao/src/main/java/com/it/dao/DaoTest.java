9fa3daeba5c54cd58f5a211f1d97c408a393a560
wangzoey968 <1324633295@qq.com>
2018-05-11 13:48:38 +0800

    null

---
+++ dao/src/main/java/com/it/dao/DaoTest.java
@@ -0,0 +1,19 @@
+package com.it.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by wangzy on 2018/5/11.
+ */
+public class DaoTest {
+    @Test
+    public void ss() throws Exception {
+       /*ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        SessionFactory factory = (SessionFactory) context.getBean("sessionFactory");
+        Session session = factory.getCurrentSession();
+        session.createQuery("from ")*/
+    }
+}
