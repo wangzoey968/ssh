062a29062c2f14f5c05db2fa8d0b84677953ac93
wangzoey968 <1324633295@qq.com>
2018-05-11 10:59:47 +0800

    null

--- pojo/src/main/java/com/it/pojo/Tb_User.java
+++ pojo/src/main/java/com/it/pojo/Tb_User.java
@@ -1,11 +1,91 @@
 package com.it.pojo;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * Created by wangzy on 2018/5/11.
  */
+@Entity
+@Table(name = "tb_user")
+public class Tb_User implements Serializable {
 
-public class Tb_User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "created",length = 255,nullable = true)
+    private Long created = System.currentTimeMillis();
 
+    @Column(name = "username",length = 255,nullable = false)
+    private String username;
 
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "remark")
+    private String remark;
+
+    @Override
+    public String toString() {
+        return "Tb_User{" +
+                "id=" + id +
+                ", created=" + created +
+                ", username='" + username + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreated() {
+        return created;
+    }
+
+    public void setCreated(Long created) {
+        this.created = created;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
