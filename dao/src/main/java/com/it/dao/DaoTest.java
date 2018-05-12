b10992d0548e2432422c6035d60fdf6d0d4218ce
wangzoey968 <1324633295@qq.com>
2018-05-12 10:52:59 +0800

    ssh

--- dao/src/main/java/com/it/dao/DaoTest.java
+++ dao/src/main/java/com/it/dao/DaoTest.java
@@ -3,17 +3,18 @@ package com.it.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by wangzy on 2018/5/11.
  */
 public class DaoTest {
-    @Test
+
     public void ss() throws Exception {
-       /*ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        SessionFactory factory = (SessionFactory) context.getBean("sessionFactory");
-        Session session = factory.getCurrentSession();
-        session.createQuery("from ")*/
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SessionFactory sessionFactory = (SessionFactory) context.getBean("sessionFactory");
+        System.out.println(sessionFactory.toString());
     }
 }
